63ae77d0c87f6d696510a803d91be6327f0e4217
mrjaimisra <misrajai01@gmail.com>
2015-07-09 14:14:19 -0600

    another solution for doing migrations in Sequel

--- db/migrate/001_create_tasks.rb
+++ db/migrate/001_create_tasks.rb
@@ -2,13 +2,8 @@ require 'sequel'
 require 'sqlite3'
 require_relative '../../app/models/task_manager'
 
-environments = %w(test development)
-
-environments.each do |environment|
-  Sequel.sqlite("db/task_manager_#{environment}.sqlite3").create_table :tasks do
+TaskManager.database.create_table :tasks do
   primary_key :id
   String      :title
   String      :description
-  end
-
 end
